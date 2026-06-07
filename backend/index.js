524978f089fb11dc924408841fd434d02a96ed93
Abhyanshu <abhyanshuab05@gmail.com>
2026-06-07 22:59:32 +0530

    fix db connection

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,11 @@ app.use(cors(
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "turasqlMine13adow@",  // replace with your MySQL password
-  database: "taskdb",
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
