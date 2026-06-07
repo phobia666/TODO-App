244e98278cf5ae44ef4276446bb16ef47e8d5bab
Abhyanshu <abhyanshuab05@gmail.com>
2026-06-07 23:03:09 +0530

    use public url

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,7 @@ app.use(cors(
 ));
 app.use(express.json());
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
   if (err) throw err;
