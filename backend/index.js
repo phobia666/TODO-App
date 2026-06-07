294cc16c531d33bcc32ad4e0f30c856f85fbe5d8
Abhyanshu <abhyanshuab05@gmail.com>
2026-06-07 22:47:40 +0530

    updated some files

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ const mysql = require("mysql2");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+  origin: 'https://phobiaytodoapp.netlify.app'
+}
+));
 app.use(express.json());
 
 const db = mysql.createConnection({
