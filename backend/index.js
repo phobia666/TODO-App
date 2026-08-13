10a17b8bd568ee0ceef6fd2caa1d0666207b3afd
Abhyanshu <abhyanshuab05@gmail.com>
2026-08-14 00:21:16 +0530

    added /root

--- backend/index.js
+++ backend/index.js
@@ -99,4 +99,9 @@ app.put("/tasks/:id", (req, res) => {
   );
 });
 
+
+app.get("/", (req, res) => {
+  res.send("TODO backend is working!");
+});
+
 app.listen(5000, () => console.log("Server running on port 5000"));
